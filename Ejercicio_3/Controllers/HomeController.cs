d9eb7948c305a68cb31c554a47b7d093cb32f40c
asd313751243 <asd313751243@gmail.com>
2020-02-16 21:34:14 -0400

    sin guardar en array

---
+++ Ejercicio_3/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ejercicio_3.Models;
+
+namespace Ejercicio_3.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Carro Notengo)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                return RedirectToAction("Quiero", Notengo);
+            }
+            return View(Notengo);
+        }
+
+        public IActionResult Quiero (Carro Notengo)
+        {
+
+            return View(Notengo);
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Ejercicio_3/Models/Carro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3.Models
+{
+    public class Carro
+    {
+        public int ID { get; set; }
+        public String NOMBRE { get; set; }
+        public String COLOR { get; set; }
+        public String MARCA { get; set; }
+        public String CHASIS { get; set; }
+
+
+    }
+}
